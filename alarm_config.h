151b1b18a39d6e92afbee2fdc4d9434221f0072e
XZman <xueyuqijim@foxmail.com>
2017-04-10 21:31:09 +0800

    update comment

--- alarm_config.h
+++ alarm_config.h
@@ -1,6 +1,6 @@
 // alarm_config.h
 
 const unsigned maxAlarmTimes = 10; // how many times the alarm rings
-const double alarmTimeRemaining = 1; // unit: minute
+const double alarmTimeRemaining = 1; // unit: minute; must be larger than 0;
 
 const unsigned long alarmTimeRemainingSecs = alarmTimeRemaining * 60;
